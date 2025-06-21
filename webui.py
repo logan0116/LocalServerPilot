8628e6f97931eee6ed9c50a6332fd93064f9407b
logan0116 <1809361150@qq.com>
2025-06-22 00:48:58 +0800

    baseupdate

--- webui.py
+++ webui.py
@@ -9,7 +9,11 @@ import time
 
 from utils import *
 
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(
+    page_title="Local Server Pilot",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
 
 
 def load_servers():
